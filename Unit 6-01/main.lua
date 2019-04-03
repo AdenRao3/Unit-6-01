30edbe47b273012c99d07f480126f3a46c6e1f20
AdenRao3 <47573320+AdenRao3@users.noreply.github.com>
2019-04-03 13:29:11 -0400

    Update main.lua

--- Unit 6-01/main.lua
+++ Unit 6-01/main.lua
@@ -7,39 +7,48 @@
 --
 -----------------------------------------------------------------------------------------
 
---backround
+--backround colour and title
+--------------
 display.setDefault( "background", 125/255, 0/255, 200/255 )
+local myText = display.newText( "Guess The \n Number!", 160, 30, native.systemFont, 45 )
+myText:setFillColor( 255/255, 100/255, 0 )
+--------------
 
+-- Calculate button and text field
+-------------
 local calculateButton = display.newImageRect( "./assets/button.png", 300, 90 )
 calculateButton.x = display.contentCenterX
-calculateButton.y = display.contentCenterY
+calculateButton.y = display.contentCenterY - 80
 calculateButton.id = " calculateButton "
  
 local answerTextField = native.newTextField( display.contentCenterX, display.contentCenterY + 200, 300, 60 )
 answerTextField.id = "answer textField"
+--------------
 
 --randomseed
 math.randomseed( os.time() )
+----------
 
+-- Functions
 local numberToGuess = math.random( 1, 2 )
 local answerAsNumber = tonumber( answerTextField.text )
+--------
 
 -- Text telling person to choose a number
-
-print( "Pick a number from 1 to 6: " )
+----------
+local myText = display.newText( "  Pick a number \nbetween 1 and 6", 160, display.contentCenterY + 40, native.systemFont, 40 )
+myText:setFillColor( 255/255, 100/255, 0 )
+----------
 
 
 local function calculateButtontouch ( event)
 
 	if numberToGuess == answerAsNumber then
-    	print( "Correct" )
+local myText = display.newText( "Correct", 160, display.contentCenterY + 40, native.systemFont, 40 )
 	else
-		print( "Wrong, guess again!")
-		print( "Correct answer was " ..numberToGuess )
+		local myText = display.newText( "                Wrong! \n The correct answer is: "..  numberToGuess, display.contentCenterX, display.contentCenterY - 50, native.systemFont, 25 ) 
 	end
-	print( "The correct answer was", numberToGuess )
 	print( "Please play again!" )
 end
-		
 
 calculateButton :addEventListener( "touch", calculateButtontouch )
